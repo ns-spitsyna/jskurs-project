2d46781634a8b4dc06a549495009f0e85b58e6fc
nadya <n.spicyna@movavi.com>
2022-09-11 14:53:00 +0700

    Произведение чисел, возведение в степень

--- additionalTask/scripts/scripts.js
+++ additionalTask/scripts/scripts.js
@@ -1,2 +1,9 @@
 'use strict';
+let num = 266219;
+let multiplicationNumbers = ('' + num).split('').reduce((sum, item) => sum*Number(item), 1); //Произведение чисел num
+
+console.log(`Произведение чисел 266219 равна: ${multiplicationNumbers}`);
+
+let result = (multiplicationNumbers**3).toString().slice(0,2); // Возведение результата в степень 3 и получение первых двух чисел
+console.log(`Первые 2 цифры полученного числа ${multiplicationNumbers**3}: ${result}`);
 
