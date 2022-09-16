3051ce9ca8b44aa5e9309366b538252b2ec6ffe3
nadya <n.spicyna@movavi.com>
2022-09-16 20:21:17 +0700

    обрезка текста

--- additionalTask/scripts/scripts.js
+++ additionalTask/scripts/scripts.js
@@ -1,2 +1,10 @@
 'use strict';
 
+function stringTrim(elem){
+    if(typeof elem != 'string') alert('Not string!');
+    let text = elem.trim();
+
+    return text.length > 30 ? text.slice(0, 30) + '...' : text
+}
+
+console.log(stringTrim('     1234567 8111111 7892 234567890 5555555 555         '));
