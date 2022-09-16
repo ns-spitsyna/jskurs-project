f9b5a394de3d42d0cbb9920cef0e84e2ea0723db
nadya <n.spicyna@movavi.com>
2022-09-16 22:21:11 +0700

    вывести натуральные числа от 2 до 100

--- additionalTask/scripts/scripts.js
+++ additionalTask/scripts/scripts.js
@@ -1,2 +1,25 @@
 'use strict';
+//1
+let arr = [];
+arr = ['22522', '44444', '35688', '111223', '994741', '456987', '4567892'];
+console.log(arr);
+for(let i = 0; i < arr.length; i++) {
+    if(arr[i].slice(0, 1) == '2' || arr[i].slice(0, 1) == '4') console.log(arr[i]);
+}
 
+//2
+
+for(let n = 2; n < 100; n++) {
+    if(isNatural(n)) {
+        console.log(`Делители этого числа: 1 и ${n}`);
+    }
+}
+
+function isNatural(number) {
+    for (let i = 2; i <= number/2; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
